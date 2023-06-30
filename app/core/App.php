1e9ef98bb6c9f972336f0a0a045839152ea473c8
jlauza <lauzajayson@gmail.com>
2023-06-30 20:26:49 +0800

    completed routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,9 +1,17 @@
 <?php
 
 class App {
-    
-    function __construct() {
-        echo "Test";
+    protected $controller = "home";
+    protected $method = "index";
+    protected $params = [];
+    public function __construct() {
+        $url = $this->parseUrl();
     }
+
+    public function parseUrl() {
+        if (isset($_GET['url'])) {
+            return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        }
+    } 
 
 }
