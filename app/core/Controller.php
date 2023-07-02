a89740a0d87a04914c47cf84e05d6c18ea2d112a
jlauza <lauzajayson@gmail.com>
2023-07-02 19:28:36 +0800

    Controller.php: replace public access modifier to protected

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -3,12 +3,12 @@
 // This file holds the parent controller
 
 class Controller {
-    public function model($model) {
+    protected function model($model) {
         require_once '../app/models/'. $model .'.php';
         return new $model();
     }
 
-    public function view($view, $data = []) {
+    protected function view($view, $data = []) {
         require_once '../app/views/'. $view .'.php';
     }
 }
